9789c054ce2633f74122d72adcc3d101885cc668
varungirikon <155966687+varungirikon@users.noreply.github.com>
2025-09-26 23:04:15 +0530

    changes

--- italian-shoes/app/layout.tsx
+++ italian-shoes/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "@/components/providers/SessionProvider";
 
 
 
